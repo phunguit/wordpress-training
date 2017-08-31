d8ce9447e4fe0d09d2a1c5a1ff27d76369285919
DESKTOP-HLKGEMR\Admin <phungthaouit@gmail.com>
2017-08-31 23:46:49 +0700

    1.23 - Widget search 02 - 4 minutes

---
+++ wp-content/themes/phung-training/inc/widgets/html/searchForm.php
@@ -0,0 +1,6 @@
+<form method="get" id="searchform" class="site-searchform" action="#" role="search">
+	<input type="search" class="field" name="s" value="" id="s" placeholder="Search..." />
+	<button type="submit">
+		<span class="fa fa-search"></span>
+	</button>
+</form>
